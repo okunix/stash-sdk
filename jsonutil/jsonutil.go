6a8325bbea057d83151048f909b873be6e9ee662
yosa12978 <yosa12978@gmail.com>
2026-04-06 01:17:18 +0300

    fix

--- jsonutil/jsonutil.go
+++ jsonutil/jsonutil.go
@@ -18,11 +18,21 @@ type Message struct {
 	Detail  any    `json:"detail"`
 }
 
+type ValidationProblems map[string]interface{}
+
+func (p ValidationProblems) Error() string {
+	errJson, _ := json.Marshal(p)
+	return string(errJson)
+}
+
 func ForgeError(r io.Reader) error {
 	errMsg := "error status code"
 	msg, err := Read[Message](r)
 	if err != nil {
 		return errors.New(errMsg)
+	}
+	if msg.Message == "Validation Error" && msg.Detail != nil {
+		return msg.Detail.(ValidationProblems)
 	}
 	if msg.Detail != nil {
 		errMsg = msg.Detail.(string)
